230337be042eaf5a6e0249691399a0b7fad139c5
Hugo Castro <javcastro@live.cl>
2026-02-05 22:57:30 -0300

    feat: 99-Configuracion de Modulos fireStore y angular

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDn7a8-qygirNfuVkx5_iI9pWQgEPDq0aI",
+        authDomain: "control-clientes-3d705.firebaseapp.com",
+        projectId: "control-clientes-3d705",
+        storageBucket: "control-clientes-3d705.firebasestorage.app",
+        messagingSenderId: "479150013016",
+        appId: "1:479150013016:web:3094d227ac6c7d535d282d"
+    }
+};
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), // Inicializa Firebase 
+    provideFirestore(() => getFirestore()),  // Inicializa Firestore 
+    provideAuth(() => getAuth()),  // Inicializa Auth 
+    provideStorage(() => getStorage()),  // Inicializa Storage 
+    ...appConfig.providers // Desestructuramos los providers del appConfig si es necesario
+  ]
+}).catch((err) => console.error(err));
