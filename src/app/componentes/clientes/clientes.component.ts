9c29b70f7f34fb146e7dca7153655778fa0cd554
Hugo Castro <javcastro@live.cl>
2026-02-09 12:20:53 -0300

    feat: 103-Componente Agregar Parte2 - Formulario nombre, apellido, emal, saldo. Definir objeto cliente

--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -3,16 +3,26 @@ import { Cliente } from '../../modelo/cliente.modelo';
 import { CommonModule } from '@angular/common';
 import { ClienteService } from '../../servicios/cliente.service';
 import { RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-clientes',
-  imports: [CommonModule, RouterModule],     //Para trabajar con currency en html se importa CommonModule y ROUTERMODULE para router linl del html 
+  imports: [CommonModule, RouterModule, FormsModule],     //Para trabajar con currency en html se importa CommonModule y ROUTERMODULE para router linl del html 
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent {
 
+
   clientes: Cliente[] | null = null; //Definimos un arreglo del tipo CLIENTE y nulo
+  //Definimos el objeto de cliente de [(ngModel)], en la variable cliente 
+  cliente: Cliente = {
+    nombre:'',
+    apellido:'',
+    email:'',
+    saldo: undefined
+  };
+
 
   constructor(private clientesServicio: ClienteService) { } //Inyectamos el servicio de clientes del tipo CLIENTESERVICE
 
@@ -24,18 +34,6 @@ export class ClientesComponent {
     });
   }
 
-  
-  // getSaldoTotal() { 
-  //   let saldoTotal: number = 0; 
-  //   if(this.clientes){ 
-  //     this.clientes.forEach( cliente => { 
-  //       if (cliente.saldo !== undefined) { 
-  //         saldoTotal += cliente.saldo; 
-  //       } 
-  //     }); 
-  //   } 
-  //   return saldoTotal; 
-  // }
   getSaldoTotal(): number {
   return this.clientes?.reduce((total, cliente) => total + (cliente.saldo ?? 0), 0) ?? 0;
   }
@@ -46,4 +44,15 @@ export class ClientesComponent {
   //1. total, que es la variable que acumula el resultado, usa el lambda para sumar el total que inicia en 0 + saldo del cliente si el saldo de cliente es nulo o indefinido su valor por defecto sera cero
   //2. 0, valor iicial de total  reduce((total, cliente).....,0)
   //Si this.clientes es cero o indefinido retornamos 0,  this.clientes?reduce() ?? 0;
+
+  agregar(clienteForm: NgForm) {
+    const {value, valid} = clienteForm; //definimos una constante en la cual recibimos el valor de nuestrom formulario y si es valido
+    if (valid){
+      //Agregamos la logoica para agregar un cliente
+
+      //limpiamos el formulario
+      clienteForm.resetForm();
+    }
+    }
 } 
+
