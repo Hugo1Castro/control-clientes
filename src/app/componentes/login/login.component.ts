73b59f3269e0e127e8b4d4c9df14d31834530066
Hugo Castro <javcastro@live.cl>
2026-02-09 23:56:45 -0300

    feat: 111-Componente Servicio de login Parte2 - Componente Login.Servicio - metodo login en componente login ts y vista con error.

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoginService } from '../../servicios/login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,30 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
   email: string | null = null; 
   password: string | null = null; 
- 
+  mensaje: string | null = null; 
+  
+  
   constructor( 
-    private router: Router 
+    private router: Router, 
+    private loginService: LoginService 
   ) { } 
  
   login(){ 
+     // Verificación de que email y password no son null 
+    if (this.email && this.password) {   
+      this.loginService.login(this.email, this.password) 
+        .then(() => { 
+          this.router.navigate(['/']); 
+        }) 
+        .catch(error => { 
+          this.mensaje = 'Error al hacer login: ' + error; 
+        }); 
+    } else { 
+      this.mensaje = 'Por favor, ingrese un email y una contraseña válidos.'; 
+    } 
   } 
-
-}
+  } 
---
+++ src/app/servicios/login.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+ // Inyectamos el servicio de autentificacion con Auth en el constructor 
+  constructor(private authService: Auth) {}  
+
+ 
+//Metodo para poder Iniciar session  
+  login(email: string, password: string) { 
+    return new Promise((resolve, reject) => { //Aquí creamos una Promesa. Una promesa es como un 'vale' por un resultado futuro. Como ir a internet tarda tiempo, yo te prometo que te daré una respuesta. Sicontraseña es correcta resolve y si falla reject
+      signInWithEmailAndPassword(this.authService, email, password) //Llamamos al método oficial de Firebase. Le pasamos nuestra conexión (authService) junto con las credenciales que el usuario escribió. Es el momento en que enviamos la información a los servidores de Google para que la verifiquen".                                                                  
+        .then(datos => resolve(datos)) //'Y entonces...'. Si Firebase nos contesta que todo está bien, recibimos los datos del usuario y activamos el resolve. Esto le avisa al resto de la aplicación: '¡Éxito, el usuario puede pasar!'
+        .catch(error => reject(error)); //Si algo sale mal (ej: contraseña incorrecta), atrapamos ese error y activamos el reject. Esto le avisa a la aplicación: 'Algo falló, muestra un mensaje de error'
+    }); 
+  }
+}
